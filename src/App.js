eee8e2db1b4ab10d7b0b613106bd71e11a9e5850
OksanaKis <oksanakisetoja@gmail.com>
2021-10-21 11:41:39 +0300

    Setting weather.name and weather.sys.country

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         <div>
           <div>
             <div className="location-box">
-            <div className="location"></div>
+            <div className="location">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
