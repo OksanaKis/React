61822f78ef8ecc6627c897cf777bc2d9dd11d991
OksanaKis <oksanakisetoja@gmail.com>
2021-10-21 10:47:59 +0300

    Cleaning working space, creating const api object, adding images.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
+const api = {
+  key: "c60621f6b01ac75d9cb4f8afef300fdc",
+  base: "http://api.openweathermap.org/data/2.5/",
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
